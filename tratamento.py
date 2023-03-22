7e953c96f214c2b2550d5db7a7bd2f6e2c6bd60d
alineralncs <alineralncs@gmail.com>
2023-03-22 15:28:12 -0300

    tentando tratar os dados e falhando miseravelmente

--- tratamento.py
+++ tratamento.py
@@ -8,29 +8,48 @@ from sklearn.ensemble import IsolationForest
 
 data = pd.read_excel('dataset.xlsx')
 
+# data.select_dtypes(exclude=['object'])
+
+data.fillna(data.median(), inplace=True)
+
+str_cols = data.select_dtypes(include=['object']).columns
+
+# print('str cols', str_cols)
+# for col in str_cols:
+
+data.drop(str_cols, axis=1, inplace=True)
+    # data.drop(col, data.median(), inplace=True)
+    # col_mean = data.loc[:, ~data.columns.isin(str_cols)][col].mean()
+    # df[col] = data[col].apply(lambda x: col_mean if isinstance(x, str) else x)
+
+
+data.drop(["Patient ID"], axis=1, inplace=True)
+# # data.to_excel('dataset.xlsx', index=False)
+
+
+print( 'data', data)
 # --------------------------------- #
 # Ele isola os valores discrepantes selecionando 
 # aleatoriamente uma característica do conjunto de características 
-# fornecido e, em seguida, selecionando aleatoriamente um valor de 
-# divisão entre o valor máximo e mínimo dessa característica. 
-# Essa divisão aleatória de características produzirá caminhos 
-# mais curtos nas árvores para os pontos de dados anômalos, 
-# distinguindo-os do resto dos dados.
+# # fornecido e, em seguida, selecionando aleatoriamente um valor de 
+# # divisão entre o valor máximo e mínimo dessa característica. 
+# # Essa divisão aleatória de características produzirá caminhos 
+# # mais curtos nas árvores para os pontos de dados anômalos, 
+# # distinguindo-os do resto dos dados.
 
 model = IsolationForest(n_estimators=100, max_samples='auto', contamination=float(0.05), random_state=42)
 
 
 # erro could not convert string to float
 # dividir as colunas em dojunto de colunas booleanas
-fit = pd.get_dummies(data)
+# data = pd.get_dummies(data)
 
 # fit.drop_duplicates()
-# meu drop na apaga todas as 
-fit =fit.dropna()
+#  drop na apaga todas as 
+# fit = fit.dropna() 
+# print(fit)
 
-print(fit)
-
-model.fit(fit)
+model.fit(data)
 
 outliers = model.predict(data)
 
@@ -38,7 +57,7 @@ outliers = model.predict(data)
 sns.heatmap(outliers.reshape(-1, 1), cmap='coolwarm', cbar=False, yticklabels=False)
 
 
-# print('d', dt)
+#print('d', dt)
 # data = data.dropna()
 
 # print('data', data)
@@ -78,7 +97,8 @@ sns.heatmap(outliers.reshape(-1, 1), cmap='coolwarm', cbar=False, yticklabels=Fa
 # desvio_hemoglobin = desvio_padrao(hemoglobin)
 
 
-# coeficiente_variacao(media_hemoglobin, desvio_hemoglobin)
+# a = coeficiente_variacao(media_hemoglobin, desvio_hemoglobin)
+# sns.heatmap(a.reshape(-1, 1), cmap='coolwarm', cbar=False, yticklabels=False)
 
 
 
